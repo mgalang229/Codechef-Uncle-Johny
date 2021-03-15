7ea9c69e31c66a942e830e329a474e9b19f957fc
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-03-15 19:47:19 +0800

    Create author_sol.cpp

---
+++ author_sol.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		vector<int> a(n);
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+		int k;
+		cin >> k;
+		// create a 'song_length' variable to store the length of Uncle Johny Song
+		int song_length = 0;
+		for (int i = 0; i < n; i++) {
+			if (i + 1 == k) {
+				// if the index + 1 is equal to the k-th position then store the value
+				song_length = a[i];
+			}
+		}
+		// create a 'position' variable to count the numbers
+		// that are less than or equal to the song length of Uncle Johny
+		int position = 0;
+		for (int i = 0; i < n; i++) {
+			if (a[i] <= song_length) {
+				// increment the variable if the current element is less than or equal to
+				// Uncle Johny's song length
+				position++;
+			}
+		}
+		cout << position << '\n';
+	}
+	return 0;
+}
